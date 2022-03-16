a1601ea23275a0dabc089b8b261a0fe79a1aa57e
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-03-16 19:20:18 +0200

    homework2 /

--- apps/computers/urls.py
+++ apps/computers/urls.py
@@ -4,5 +4,5 @@ from apps.computers.views import ComputerListCreateView, ReadUpdateDeleteView
 
 urlpatterns = [
     path('', ComputerListCreateView.as_view(), name='computers_list_create'),
-    path('<int:pk>', ReadUpdateDeleteView.as_view(), name='computers_read_update_delete')
+    path('<int:pk>/', ReadUpdateDeleteView.as_view(), name='computers_read_update_delete')
 ]
