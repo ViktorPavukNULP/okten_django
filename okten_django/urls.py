dd8c26ad7d65adf8239892ee8c3963955d43e95a
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-03-23 21:27:19 +0200

    homework3 slash fix

--- okten_django/urls.py
+++ okten_django/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('cars', include('apps.cars.urls')),
-    path('auto_parks', include('apps.autopark.urls'))
+    path('cars/', include('apps.cars.urls')),
+    path('auto_parks/', include('apps.autopark.urls'))
 ]
