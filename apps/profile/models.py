1f47ab08801ad21786899044daeb935c15f2d3e4
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-03-30 21:19:41 +0300

    homework5 profile validation

--- apps/profile/models.py
+++ apps/profile/models.py
@@ -3,11 +3,20 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 UserModel = get_user_model()
+
+
 class ProfileModel(models.Model):
     class Meta:
         db_table = 'profile'
-    name = models.CharField(max_length=30)
-    surname = models.CharField(max_length=30)
+
+    name = models.CharField(max_length=30, validators=(
+        RegexValidator(r'^([A-Z]{1}[a-z]{1,19})$',
+                       'Name must start with the capital letter and contain from 2 to 20 letters. Example: Andriy'),
+    ))
+    surname = models.CharField(max_length=30, validators=(
+        RegexValidator(r'^([A-Z]{1}[a-z]{1,19})$',
+                       'Surname must start with the capital letter and contain from 2 to 20 letters. Example: Popov'),
+    ))
     born = models.DateField()
     phone = models.CharField(max_length=13, validators=(
         RegexValidator(r'^\+380[\d]{9}$', 'Invalid phone number. Example: +380xxxxxxxxx'),
