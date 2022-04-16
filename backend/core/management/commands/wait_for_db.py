c0438de7013acb0692a79b8a37e35ad6fe42924e
ViktorPavukNULP <viktor.pavuk.kn.2017@lpnu.ua>
2022-04-16 14:11:13 +0300

    homework9 core

---
+++ backend/core/management/commands/wait_for_db.py
@@ -0,0 +1,19 @@
+import time
+
+from django.core.management.base import BaseCommand
+from django.db import OperationalError, connection
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write('wait for db...')
+        db_con = False
+        while not db_con:
+            try:
+                connection.ensure_connection()
+                db_con = True
+            except OperationalError:
+                self.stdout.write('Database is not ready, wait 1 second...')
+                time.sleep(1)
+        self.stdout.write('Database is available')
+
